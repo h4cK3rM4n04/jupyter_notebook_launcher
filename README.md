a84107f7fd94d8d622888e6df3aff0a12b59f4cc
h4cK3rM4n04 <eliemanoarm@gmail.com>
2024-02-11 21:53:02 +0100

    jupyter notebook launcher

---
+++ README.md
@@ -0,0 +1,1 @@
+#Just run the file if you have "jupyter notebook" on local
---
+++ keyboard_control_1.py
@@ -0,0 +1,17 @@
+#coding:utf-8
+import keyboard
+from time import sleep
+
+#ASK IF OCEANE DO KARATE ON MESSENGER !!!
+
+
+
+keyboard.press_and_release("win+r")
+sleep(0.100)
+for i in "cmd":
+	keyboard.press_and_release(i)
+keyboard.press_and_release("enter")
+sleep(0.14)
+for x in 'jupyter notebook':
+	keyboard.press_and_release(x)
+keyboard.press_and_release("enter")
